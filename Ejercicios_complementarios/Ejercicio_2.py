61a07ba628397bbba9f2e9028c484ab3100fb231
Sofiablangetti <sofavoleycap@gmail.com>
2025-09-03 20:21:38 -0300

    corregi

--- Ejercicios_complementarios/Ejercicio_2.py
+++ Ejercicios_complementarios/Ejercicio_2.py
@@ -2,5 +2,3 @@ numero1 = int(input("Ingrese un numero entero: "))
 print("su numero entero es", numero1)
 numero2 = float(input("Ingrese un numero decimal: "))
 print("su numero decimal es", numero2)
-suma = numero1 + numero2
-print("La suma es:", suma)
